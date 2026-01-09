c4ac0c0cbcb6293aab8c2e80202c9fba678792e2
Hazel Remmen <jurian.remmen@gmail.com>
2026-01-09 20:16:24 +0100

    require

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <h1></h1>
 
     
-<?php include '/partials/navbar.php'; ?>
+<?php require '/partials/navbar.php'; ?>
 
 <div id="message">
   <h1 id="message-title">A story about the order of time</h1>
