f864717a3e34faed8366319ea2bc61eb44734023
Hazel Remmen <jurian.remmen@gmail.com>
2026-01-19 09:35:18 +0100

    Update navbar, BEFORE using AI to restructure

--- public/index.php
+++ public/index.php
@@ -18,7 +18,7 @@
     <h1></h1>
 
     
-<?php require __DIR__ . '/partials/navbar.php'; ?>
+<?php require __DIR__ . '/../partials/navbar.php'; ?>
 
 <div id="message">
   <h1 id="message-title">A story about the order of time</h1>
