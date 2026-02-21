11403f537eb49ceaee56c866650129f1af4a90d6
Hazel Remmen <jurian.remmen@gmail.com>
2026-02-21 14:36:56 +0100

    Restructure for Jinja

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from jinja2 import Environment, FileSystemLoader
+from random import random
+
+env = Environment(
+    loader=FileSystemLoader('/var/www/hazelremmen/templates'),
+    auto_reload=True
+)
+
+def application(environ, start_response):
+    template = env.get_template("index.html")
+
+    html = template.render(
+        random_num=random()
+    )
+
+    start_response("200 OK", [("Content-Type", "text/html")])
+    return [html.encode("utf-8")]
