2da19574a1e49794d412c54a1229c777d56d5cca
Hazel Remmen <jurian.remmen@gmail.com>
2026-02-21 23:12:55 +0100

    Navbar changes

--- main/routes.py
+++ main/routes.py
@@ -1,11 +1,17 @@
 from flask import render_template
 from random import random
 
+class user:
+    def __init__(self):
+        self.logged_in=False
+        self.name=""
+        self.id=""
+
 def init_routes(app):
     @app.route('/')
     def index():
-        return render_template("base.html", main_content="blog", blog_posts=["001","002","003","004"])
+        return render_template("base.html", main_content="blog", user=user, blog_posts=["001","002","003","004"], random_num=random())
     
     @app.route('/motd')
     def motd():
-        return render_template("base.html", main_content="motd")
+        return render_template("base.html", main_content="motd", user=user)
