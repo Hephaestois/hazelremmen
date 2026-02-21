e7efed4384ae9fd9d8a25b219284715fced1e5f2
Hazel Remmen <jurian.remmen@gmail.com>
2026-02-21 19:37:53 +0100

    Make blog posts included

--- main/routes.py
+++ main/routes.py
@@ -4,7 +4,7 @@ from random import random
 def init_routes(app):
     @app.route('/')
     def index():
-        return render_template("base.html", main_content="blog", random_num=random())
+        return render_template("base.html", main_content="blog", blog_posts=["001","002","003","004"])
     
     @app.route('/motd')
     def motd():
