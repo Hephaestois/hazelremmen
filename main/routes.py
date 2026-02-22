70d08c47e3dc9ef1223ff0fd0debaea5cca7ee0c
Hazel Remmen <jurian.remmen@gmail.com>
2026-02-22 10:59:34 +0100

    remove duplicate routing?

--- main/routes.py
+++ main/routes.py
@@ -16,13 +16,13 @@ def init_routes(app):
     def motd():
         return render_template("base.html", main_content="motd", user=user)
     
-    @app.route('/auth/login', methods=("GET", "POST"))
-    def login():
-        return render_template("base.html", main_content="auth/login", user=user)
+    # @app.route('/auth/login', methods=("GET", "POST"))
+    # def login():
+    #     return render_template("base.html", main_content="auth/login", user=user)
     
-    @app.route('/auth/register', methods=("GET", "POST"))
-    def register():
-        return render_template("base.html", main_content="auth/register", user=user)
+    # @app.route('/auth/register', methods=("GET", "POST"))
+    # def register():
+    #     return render_template("base.html", main_content="auth/register", user=user)
     
     
     
